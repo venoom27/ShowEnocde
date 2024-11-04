83e9ec34643a71c2a88fb0e68a688e5415f4a638
venoom27 <57874529+venoom27@users.noreply.github.com>
2024-11-04 15:24:15 -0500

    Create StartShowEncode.bat

---
+++ StartShowEncode.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+TIMEOUT /NOBREAK /T 21600  "Script timed out after 6 hours."
+
+C:\ffmpeg\Python\python.exe "E:\Batch files\ShowEncoding.py"
+
+ECHO Script execution completed.
+
+rem PAUSE
